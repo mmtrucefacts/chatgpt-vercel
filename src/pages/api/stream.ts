4ac3c359b6bb07cb91ed707200244d90141b53ea
mmtrucefacts <93686164+mmtrucefacts@users.noreply.github.com>
2023-03-16 01:13:51 +0000

    Update stream.ts

--- src/pages/api/stream.ts
+++ src/pages/api/stream.ts
@@ -21,10 +21,10 @@ export const post: APIRoute = async context => {
 
   if (!key.startsWith("sk-")) key = apiKey
   if (!key) {
-    return new Response("没有填写 OpenAI API key")
+    return new Response("OpenAI API Key Not Detected, Press / To Find Your Key And Imput It In The Settings")
   }
   if (!messages) {
-    return new Response("没有输入任何文字")
+    return new Response("No Key")
   }
 
   const completion = await fetch("https://api.openai.com/v1/chat/completions", {
